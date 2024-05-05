d17a5b2e962aa799c2042e82a09b740e8595dd0c
sandeepkumaar <cskumaar1992@gmail.com>
2024-05-05 23:26:33 +0530

    fix types

--- src/predicates/tiny-schema-wrapper.js
+++ src/predicates/tiny-schema-wrapper.js
@@ -1,15 +1,18 @@
 // @ts-ignore
 import tinySchema from "tiny-schema";
 
-const getError = (key, value, condition) => {
+/** @param {any} value*/
+const getError = (key = "", value, condition = "") => {
   let e = TypeError(
     `Expected {${key}} to satisfy {${condition}} validation. Given {${key}: ${value}}`,
   );
+  // @ts-ignore
   e.predicate = condition;
   return e;
 };
 
-export function is(scheme) {
+export function is(scheme = "") {
+  /** @param {any} value*/
   return function (value, key = "input") {
     if (tinySchema(scheme)(value)) return value;
     throw getError(key, value, scheme);
